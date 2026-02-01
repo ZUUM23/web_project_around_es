d167ede6f13d62a7182438762821cccbf158ce02
alejandro fernandez <manuel3053303697@gmail.com>
2026-02-01 14:27:38 -0500

    sprint 8 parte 2

--- scripts/index.js
+++ scripts/index.js
@@ -31,9 +31,16 @@ initialCards.forEach(function (card) {
 const openCardButtonEdit = document.querySelector(".profile__edit-button");
 const divCard = document.querySelector(".popup");
 const openCardButton = document.querySelector("#edit-popup");
+const profileForm = document.querySelector(".popup__form");
 const closeCardButton = openCardButton.querySelector(".popup__close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const initialName = document.querySelector(".popup__input_type_name");
+const descriction = document.querySelector(".popup__input_type_description");
 
+// declarar
 const openModal = (modal) => {
+  fillProfileForm();
   divCard.classList.add(`popup_is-opened`);
 };
 
@@ -41,5 +48,28 @@ const closeModal = (modal) => {
   divCard.classList.remove(`popup_is-opened`);
 };
 
-openCardButtonEdit.addEventListener("click", openModal(openCardButton));
-closeCardButton.addEventListener("click", closeModal(openCardButton));
+function fillProfileForm() {
+  const currentName = profileTitle.textContent;
+  const currentDescription = profileDescription.textContent;
+
+  initialName.value = currentName;
+  descriction.value = currentDescription;
+}
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(openCardButton);
+}
+
+const handleProfileFormSubmit = (evt) => {
+  evt.preventDefault();
+  const newName = initialName.value;
+  const newdescription = descriction.value;
+
+  profileTitle.textContent = newName;
+  profileDescription.textContent = newdescription;
+  closeModal(openCardButton);
+};
+
+openCardButtonEdit.addEventListener("click", handleOpenEditModal);
+closeCardButton.addEventListener("click", closeModal);
+profileForm.addEventListener("submit", handleProfileFormSubmit);
